2329bd578cb0a958be183e6a886e8d7ff7cc7f13
swvanderlaan <s.w.vanderlaan@gmail.com>
2021-11-24 23:36:12 +0100

    adding more testing

--- test.pyhist.sh
+++ test.pyhist.sh
@@ -2,10 +2,7 @@
 
 # Testig PyHIST
 
-OUTPUT="/Users/slaan3/git/PyHIST/output"
-IMAGEFILE="AE1468.CD68"
-UPGRADESTUDY="/Users/slaan3/git/swvanderlaan/slideToolKit/upgrade_study"
-STAIN="CD68"
+EXPRESSHIST="/Users/slaan3/git/ExpressHIST"
 
 # for M in graph adaptive otsu ; do
 # 	METHOD=$M
@@ -82,20 +79,30 @@ STAIN="CD68"
 # 	
 # done
 
-for M in adaptive ; do
-	METHOD=$M
-	### DEBUG
-	### ls -lh ${UPGRADESTUDY}/${STAIN}/${IMAGEFILE}/${IMAGEFILE}.TIF
-	
-	echo "Testing method [ $M ]..."
-# 	--corners 1011 --borders 0000 --percentage-bc 5 \	
-	python3 pyhist.py --content-threshold 0.20 \
-	--patch-size 512 --output-downsample 4 --info "verbose" \
-	--method "$METHOD" \
-	--save-patches --save-mask --save-tilecrossed-image \
-	--save-edges --save-blank \
-	${UPGRADESTUDY}/${STAIN}/${IMAGEFILE}/${IMAGEFILE}.TIF
+for METHOD in adaptive; do 
+	for PATCHSIZE in 2000 1024 512 256; do 
+		for CONTENTTHRESHOLD in 0.20 0.15 0.10 0.05; do 
+			for OUTPUTDOWN 2 4 16; do 
+				for IMAGE in AE3606.GLYCC AE3756.GLYCC AE3884.GLYCC AE4022.GLYCC AE4611.GLYCC AE4612.GLYCC ; do
 
-	mv -v ${OUTPUT}/${IMAGEFILE} ${OUTPUT}/${IMAGEFILE}_${METHOD}_ct20_cornersDEF_percbcDEF
-	
+					echo "Testing method [ $METHOD ] for image [ $IMAGE ] at patchsize [ $PATCHSIZE ] with content threshold [ $CONTENTTHRESHOLD ] and down sampling [ $OUTPUTDOWN ]..."
+				# 	--corners 1011 --borders 0000 --percentage-bc 5 \	
+					python3 $EXPRESSHIST/pyhist.py --content-threshold $CONTENTTHRESHOLD \
+					--patch-size $PATCHSIZE --output-downsample $OUTPUTDOWN --info "verbose" \
+					--method "$METHOD" \
+					--save-patches --save-mask --save-tilecrossed-image \
+					$(pwd)/${IMAGE}/${IMAGE}.TIF
+
+					mv -v $(pwd)/${IMAGE}/${IMAGE}.tiles $(pwd)/${IMAGE}.${METHOD}.p${PATCHSIZE}.ct${CONTENTTHRESHOLD}.o${OUTPUTDOWN}/
+					mv -v $(pwd)/${IMAGE}/mask_* $(pwd)/${IMAGE}.${METHOD}.p${PATCHSIZE}.ct${CONTENTTHRESHOLD}.o${OUTPUTDOWN}/
+					mv -v $(pwd)/${IMAGE}/tile_selection.tsv $(pwd)/${IMAGE}.${METHOD}.p${PATCHSIZE}.ct${CONTENTTHRESHOLD}.o${OUTPUTDOWN}/
+					mv -v $(pwd)/${IMAGE}/tilecrossed_* $(pwd)/${IMAGE}.${METHOD}.p${PATCHSIZE}.ct${CONTENTTHRESHOLD}.o${OUTPUTDOWN}/
+				done
+			done
+		done
+	done
 done
+
+
+
+
